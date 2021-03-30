38f722d6510ee21041c1f52a18218128bcfa0388
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2021-03-30 15:15:53 -0400

    Add logic to receive props

--- components/Chat.js
+++ components/Chat.js
@@ -2,14 +2,32 @@ import React from "react";
 import { View, Text } from "react-native";
 
 export default class Chat extends React.Component {
-	render() {
+	componentDidMount() {
 		let name = this.props.route.params.name; // OR ...
 		// let { name } = this.props.route.params;
 
 		this.props.navigation.setOptions({ title: name });
+	}
+
+	render() {
+		let { name, bgcolor } = this.props.route.params; //destructure props and use as variables
+
 		return (
-			<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-				<Text>Hello Screen2!</Text>
+			<View
+				style={{
+					flex: 1,
+					justifyContent: "center",
+					alignItems: "center",
+					backgroundColor: bgcolor,
+				}}
+			>
+				<Text
+					style={{
+						color: bgcolor === "#090C08" || bgcolor === "#474056" ? "#fff" : "#000", //Check colors and if they are the dark ones, make text color white.
+					}}
+				>
+					Hello {name}!
+				</Text>
 			</View>
 		);
 	}
