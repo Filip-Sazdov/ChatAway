30d2315321c49fbaa16634f005136c869ddcdf65
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2021-03-30 20:49:18 -0400

    Clean redundant code

--- App.js
+++ App.js
@@ -12,11 +12,6 @@ import Chat from "./components/Chat";
 const Stack = createStackNavigator();
 
 export default class App extends Component {
-	// constructor(props) {
-	// 	super(props);
-	// 	this.state = { text: "" };
-	// }
-
 	render() {
 		return (
 			<NavigationContainer>
@@ -25,23 +20,6 @@ export default class App extends Component {
 					<Stack.Screen name="Chat" component={Chat} />
 				</Stack.Navigator>
 			</NavigationContainer>
-			// <ScrollView>
-			// 	<Text style={{ fontSize: 110 }}>This text is so big! And so long! You have to scroll!</Text>
-			// </ScrollView>
-			// <View style={styles.container}>
-			// 	<View style={styles.box1}></View>
-			// 	<View style={styles.box2}>
-			// 		<TextInput
-			// 			style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-			// 			onChangeText={(text) => this.setState({ text })}
-			// 			value={this.state.text}
-			// 			placeholder="Type here ..."
-			// 		/>
-			// 		<Text>You wrote: {this.state.text}</Text>
-			// 		<Button title="Press me..." onPress={() => this.alertMyText({ text: this.state.text })} />
-			// 	</View>
-			// 	<View style={styles.box3}></View>
-			// </View>
 		);
 	}
 }
