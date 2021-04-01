948f4dc46b40ffbaa55628cdc7077e29f4431807
Filip Sazdov <45643632+Filip-Sazdov@users.noreply.github.com>
2021-03-31 23:45:42 -0400

    Import GiftedChat

--- components/Chat.js
+++ components/Chat.js
@@ -1,12 +1,33 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
 
 export default class Chat extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			messages: [],
+		};
+	}
 	componentDidMount() {
 		let name = this.props.route.params.name; // OR ...
 		// let { name } = this.props.route.params;
-
 		this.props.navigation.setOptions({ title: name });
+
+		this.setState({
+			messages: [
+				{
+					_id: 1,
+					text: "Hello developer",
+					createdAt: new Date(),
+					user: {
+						_id: 2,
+						name: "React Native",
+						avatar: "https://placeimg.com/140/140/any",
+					},
+				},
+			],
+		});
 	}
 
 	render() {
